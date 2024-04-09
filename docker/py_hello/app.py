ca75c2c2503f3533d96c4d4106d3ad9499e43ab5
Chris Graham <chrisg@chriss-mbp.home>
2024-04-09 12:08:02 +0200

    initial

--- docker/py_hello/app.py
+++ docker/py_hello/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Worldb7!'
+    return 'Hello, Worldb8!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
